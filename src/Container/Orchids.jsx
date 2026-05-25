7c15001200386520551c7f4f48229c0837c58a65
unknown <Nv_An>
2026-05-25 17:50:24 +0700

    Update Orchids.jsx and orchid.css

--- src/Container/Orchids.jsx
+++ src/Container/Orchids.jsx
@@ -6,17 +6,19 @@ import { toast } from 'react-hot-toast';
 export default function Orchids() {
 
     const [orchidProfile, setOrchidProfile] = useState({
-        id: 0,
+        id: "",
         name: "",
-        rating: 0,
+        rating: "",
         image: "",
     })
     // update new orchid
     const [updateOrchid, setUpdateOrchid] = useState({
-        id: 0,
+        id: "",
         name: "",
-        rating: 0
+        rating: "",
     })
+
+
 
     const [showList, setShowList] = useState(false)
 
@@ -32,95 +34,114 @@ export default function Orchids() {
 
     return (
         <>
-        <div className="container">
-            <div className="Profile">
-                <h1>Orchid Profile</h1>
-                <p>
-                    Orchid ID:
-                    <input value={orchidProfile.id} onChange={(e) => setOrchidProfile({
-                        ...orchidProfile,
-                        id: e.target.value
-                    })} />
-                </p>
+            <div className="profile-wrapper">
+                <div className="Profile">
+                    <h1>Orchid Profile</h1>
+                    <div className="input-field">
 
-                <p>
-                    Orchid Name:
-                    <input value={orchidProfile.name} onChange={(e) => setOrchidProfile({
-                        ...orchidProfile,
-                        name: e.target.value
-                    })} />
-                </p>
+                        <input id="id" required type="text" value={orchidProfile.id} onChange={(e) => setOrchidProfile({
+                            ...orchidProfile,
+                            id: e.target.value
+                        })} />
+                        <label htmlFor="id">Enter ID</label>
+                    </div>
 
-                <p>
-                    Orchid Rating:
-                    <input value={orchidProfile.rating} onChange={(e) => setOrchidProfile({
-                        ...orchidProfile,
-                        rating: e.target.value
-                    })} />
-                </p>
+                    <div className="input-field">
+
+                        <input id="name" required type="text" value={orchidProfile.name} onChange={(e) => setOrchidProfile({
+                            ...orchidProfile,
+                            name: e.target.value
+                        })} />
+                        <label htmlFor="name">Enter Name</label>
+                    </div>
+
+                    <div className="input-field">
+
+                        <input id="rating" required type="text" value={orchidProfile.rating} onChange={(e) => setOrchidProfile({
+                            ...orchidProfile,
+                            rating: e.target.value
+                        })} />
+                        <label htmlFor="rating">Rating</label>
+                    </div>
+                    <button onClick={handleSearch} className="button">Search Orchid </button>
+                </div>
+
                 {orchidProfile.image && (
-                    <p>
+                    <div className="img-profile">
                         <img src={orchidProfile.image} alt={orchidProfile.name} className="image" />
-                    </p>
+                    </div>
                 )}
-
-
-                <button onClick={handleSearch} className="search-button">Search Orchid </button>
             </div>
 
-            <div className="container-update">
-                <h1>Update Orchid</h1>
+            <div className="Update-wrapper">
+                <div className="Update">
+                    <h1>Update Orchid</h1>
 
 
-                <p>
-                    Orchid ID:
-                    <input value={updateOrchid.id} onChange={(e) => setUpdateOrchid({
-                        ...updateOrchid,
-                        id: e.target.value
-                    })} />
-                </p>
+                    <div className="input-field">
 
-                <p>
-                    Orchid Name:
-                    <input value={updateOrchid.name} onChange={(e) => setUpdateOrchid({
-                        ...updateOrchid,
-                        name: e.target.value
-                    })} />
-                </p>
+                        <input id="update-id" required type="text" value={updateOrchid.id} onChange={(e) => setUpdateOrchid({
+                            ...updateOrchid,
+                            id: e.target.value
+                        })} />
+                        <label htmlFor="update-id">Enter ID</label>
+                    </div>
 
-                <p>
-                    Orchid Rating:
-                    <input value={updateOrchid.rating} onChange={(e) => setUpdateOrchid({
-                        ...updateOrchid,
-                        rating: e.target.value
-                    })} />
-                </p>
+                    <div className="input-field">
 
-                <button onClick={() => setOrchidProfile(updateOrchid)}>Update new Orchid</button>
+                        <input id="update-name" required type="text" value={updateOrchid.name} onChange={(e) => setUpdateOrchid({
+                            ...updateOrchid,
+                            name: e.target.value
+                        })} />
+                        <label htmlFor="update-name">Enter Name</label>
+                    </div>
+
+                    <div className="input-field">
+
+                        <input id="update-rating" required type="text" value={updateOrchid.rating} onChange={(e) => setUpdateOrchid({
+                            ...updateOrchid,
+                            rating: e.target.value
+                        })} />
+                        <label htmlFor="update-rating">Enter Rating</label>
+                    </div>
+
+                    <button onClick={() => setOrchidProfile(updateOrchid)} className="button">Update new Orchid</button>
 
 
+                </div>
             </div>
 
-            
-        </div>
-        <button onClick={() => setShowList(!showList)} className="list-button">{showList ? "Hide List" : "Show List"}</button>
+
+
+
+            <div className="button-list">
+                <button onClick={() => setShowList(!showList)} className="button">{showList ? "Hide List" : "Show List"}</button>
+            </div>
 
             {showList && (
                 <div className="list">
                     <h2>Danh sách hoa lan có sẵn</h2>
-                    <ul>
+                    <div className="palette">
                         {orchids.map((orchid) => (
-                            <li key={orchid.id}>
-                                ID: {orchid.id} - {orchid.name} - ⭐  {orchid.rating} -
-                                {orchid.image && (
-                                    <img src={orchid.image} alt={orchid.name} className="image" />
-                                )}
-
-                            </li>
+                            <div className="color" key={orchid.id}>
+                                <div className="orchid-card">
+                                    <img src={orchid.image} alt={orchid.name} />
+                                    <span>ID: {orchid.id}</span>
+                                    <span>{orchid.name}</span>
+                                    <span>{orchid.rating}⭐</span>
+                                </div>
+                            </div>
                         ))}
-                    </ul>
+                    </div>
+                    <div id="stats">
+                        <span>{orchids.length} orchids</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                            <path d="M4 7.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5S5.5 9.83 5.5 9 4.83 7.5 4 7.5zm10 0c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm-5 0c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5S9.83 7.5 9 7.5z"></path>
+                        </svg>
+                    </div>
                 </div>
             )}
-            </>
+
+        </>
     )
 }
